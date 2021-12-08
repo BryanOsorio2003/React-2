10c35500b548dedc766668cc56301431e1dc8d03
bryanosorio2003 <bsosorio550@misena.edu.co>
2021-12-08 15:13:25 -0500

    select lleno con los valores de la api(nombre)

--- src/Components/UI/Select/Select.jsx
+++ src/Components/UI/Select/Select.jsx
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useState } from 'react';
 import './Select.css';
-// const url = 'https://rickandmortyapi.com/api/character'    
 
 
-// const personajes = [];
+// const personajes = []; 
+
 export const Select=()=> {
-  
+  const [data,setCharacters] = useState([])
+  const url = 'https://rickandmortyapi.com/api/character' 
+  fetch(url)
+  .then(response => response.json())
+  .then(data=>setCharacters(data.results));
     return(
+
+      <>
       <select name="Select" id="Select">
         <option value="">select a character</option>
         <option value="">All characters</option>
+        {
+          data.map((option)=>(
+            <option value={option}>{option.name}</option>
+          ))
+        }
       </select>
-    )
-}
+      </>
+      )
+    }
