eae809b8024cc0ef3e836d21e41c745939b9e7c5
bryanosorio2003 <bsosorio550@misena.edu.co>
2021-12-09 20:26:53 -0500

    otra

--- src/Components/Page/Index.jsx
+++ src/Components/Page/Index.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-// import { Main } from "../Layout/Main/Main"
-import { Header } from "../Layout/Header/Header"
+import { Main } from "../Layout/Main/Main"
+// import { Header } from "../Layout/Header/Header"
 
 export const Index = () =>{
     return(
         <>
                 <p>Hola malparido error de mierda, ¿dónde estás? te encontraré, te amo.</p>
-                <Header/>
-                {/* <Main/> */}
+                {/* <Header/> */}
+                <Main/>
         </>
     )
 }
